8b239353053340d424c8a5d369b2deb1e4bd9bd3
Clara <132680946+mariaclara5842@users.noreply.github.com>
2024-03-15 11:40:06 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+const botao = document.querySelectorAll(".botao");
+`__________________________________`
+for (let i=0;i<botao.length;i++){
+ `__________________`
+    if(i==0){
+        fundo.classList.remove("fundo-branco");
+        fundo.classList.add("fundo-preto");
+    } else{
+      fundo.classList.remove("fundo-preto");
+     `__________________`
+    }  }  
