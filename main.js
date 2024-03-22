c965d3f2cb79f4a431ad169101888e0843d97cdc
Clara <132680946+mariaclara5842@users.noreply.github.com>
2024-03-22 11:48:53 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
-const botao = document.querySelectorAll(".botao");
-`__________________________________`
-for (let i=0;i<botao.length;i++){
- `__________________`
-    if(i==0){
-        fundo.classList.remove("fundo-branco");
-        fundo.classList.add("fundo-preto");
-    } else{
-      fundo.classList.remove("fundo-preto");
-     `__________________`
-    }  }  
+const botao = document.querySelectorAll("
+     const textos = document.querySelectorAll(".aba-conteudo");
+
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
+
+        for (let j = 0; j < botoes.length; j++) {
+            botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
+        }
+
+        botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
+    }
+}
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+let tempoAtual = new Date();
+
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+                                   
